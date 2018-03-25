c41aaff3b7a16f9a0d46fb601d4cbd5615456079
zalachenko <stanleyswierczek@gmail.com>
2018-03-24 23:33:20 -0700

    plot fixes

--- LaplacePlot.m
+++ LaplacePlot.m
@@ -6,27 +6,27 @@ Q = sort(True);
 if(Q(end)-Q(1)>1e5)
     
     figure
-    semilogy(T,True,'LineWidth',3,'Color','black')
+    semilogy(T,True,'LineWidth',2.5,'Color',Color(:,11))
     hold on
-    semilogy(T,NAB,'+','MarkerSize',8,'Color',Color(:,9))
-    title(['L',num2str(char),'_cl=',num2str(2*ub)])
+    semilogy(T,NAB,'*','MarkerSize',7,'Color',Color(:,12))
+    title(['L',num2str(char),' cl=',num2str(2*ub)])
     xlabel('time')
     ylabel('f(t)')
     legend('True f(t)','Bromwich adapt')
-    print(['L',num2str(char),'_cl=',num2str(2*ub)],'-djpeg')
+    print(['L',num2str(char),' cl=',num2str(2*ub)],'-djpeg')
     hold off
     
 else
     
     figure
-    plot(T,True,'LineWidth',3,'Color','black')
+    plot(T,True,'LineWidth',2.5,'Color',Color(:,11))
     hold on
-    plot(T,NAB,'+','MarkerSize',8,'Color',Color(:,9))
-    title(['L',num2str(char),'_cl=',num2str(2*ub)])
+    plot(T,NAB,'*','MarkerSize',7,'Color',Color(:,12))
+    title(['L',num2str(char),' cl=',num2str(2*ub)])
     xlabel('time')
     ylabel('f(t)')
     legend('True f(t)','Bromwich adapt')
-    print(['L',num2str(char),'_cl=',num2str(2*ub)],'-djpeg')
+    print(['L',num2str(char),' cl=',num2str(2*ub)],'-djpeg')
     hold off
     
 end
@@ -35,12 +35,11 @@ BromRelError = abs(NAB-True)./abs(True);
 error = mean(BromRelError);
 
 figure
-semilogy(T,real(BromRelError),'+','MarkerSize',8,'Color',Color(:,9))
-title(['L',num2str(char),'_cl=',num2str(2*ub),'_error'])
+semilogy(T,real(BromRelError),'*','MarkerSize',7,'Color',Color(:,12))
+title(['L',num2str(char),' cl=',num2str(2*ub),' error'])
 xlabel('time')
 ylabel('Relative error')
-print('L1error','-djpeg')
-print(['L',num2str(char),'_cl=',num2str(2*ub),'_error'],'-djpeg')
+print(['L',num2str(char),' cl=',num2str(2*ub),' error'],'-djpeg')
 hold off
 
 end
--- test9.m
+++ test9.m
@@ -11,12 +11,12 @@ colors
 %% setting up time values and shifts
 a = 1;
 b = 2;
-t = 3;
-[n,m] = size(t);
+t = 3:10;
 ub = 5;
-start = 11;
-num_test = 20;
-error = zeros(num_test-start+1,1);
+start = 61;
+num_test = 63;
+error = zeros(num_test-start+1,2);
+counter = 1;
 %%
 
 %% tests
@@ -24,15 +24,18 @@ for jj=start:num_test
     True = master_inverse_laplace_fcn(t,a,b,jj);
     fun = @(x)master_laplace_fcn(x,a,b,jj);
     NAB = nabilt(fun,t,ub);
-    error(jj,1) = LaplacePlot(True,NAB,t,jj,ub);
+    error(counter,:) = [LaplacePlot(True,NAB,t,jj,ub),jj];
+    counter = counter + 1;
 end
 %%
 
 %% error summary
 % fprintf('max relative error = %g\n',max(error))
 % fprintf('mean relative error = %g\n',mean(error))
-fprintf('max relative error = %g percent\n',100*max(error))
-fprintf('mean relative error = %g percent\n',100*mean(error))
+fprintf('max relative error = %g percent\n',100*max(error(:,1)))
+fprintf('mean relative error = %g percent\n',100*mean(error(:,1)))
 %%
+
+error
 
 toc()
